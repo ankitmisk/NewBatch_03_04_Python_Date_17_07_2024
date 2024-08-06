af79e4aaa97c0559a08e4182533f84f46b2b3f8f
Ankit Mishra <116267683+ankitmisk@users.noreply.github.com>
2024-08-06 15:58:42 +0530

    Add files via upload

---
+++ shanu.py
@@ -0,0 +1,11 @@
+# This is python code file
+
+name = input("Enter your name: ")
+
+print('Hello',name,'How are you??')
+
+print('We are in Techvidya')
+
+print('We are learning Python!!')
+
+print("Bye Bye!!👍👍")
